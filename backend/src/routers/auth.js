6a23b27e9d2c30cac32750d4e4477e20c44e4c51
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-12-30 20:55:08 +0200

    Created signin

--- backend/src/routers/auth.js
+++ backend/src/routers/auth.js
@@ -2,8 +2,8 @@ const express = require("express");
 const User = require("../models/User.js");
 const router = express.Router();
 
-checkConflict = async function (user) {
-    const existing_user = await User.findOne({ user_name: user.user_name });
+checkConflict = async function (username) {
+    const existing_user = await User.findOne({ user_name: username });
     if (existing_user) {
         return true;
     }
@@ -12,7 +12,7 @@ checkConflict = async function (user) {
 
 generateUserObject = async function (user) {
     try {
-        const userObj = {
+        const user_object = {
             user_name: user.user_name,
             password: user.password,
             first_name: user.first_name,
@@ -23,16 +23,31 @@ generateUserObject = async function (user) {
             email_address: user.email_address,
             role: user.role
         };
-        return userObj;
+        return user_object;
     } catch (error) {
         return null;
     }
-}
+};
+
+verifyCredentials = async function (username_email, password) {
+    try {
+        const user = await User.findOne({ $or: [{ email_address: username_email }, { user_name: username_email }] })
+        if (user && password === user.password) {
+            return user;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        return null;
+    }
+
+};
+
 router.post("/auth/signup", async (req, res) => {
     try {
         const user = new User(req.body);
 
-        if (!(await checkConflict(user))) {
+        if (!(await checkConflict(user.user_name))) {
             const saved_user = await user.save();
             if (!saved_user) {
                 return res.status(400).send({ error: "User not saved" });
@@ -53,4 +68,31 @@ router.post("/auth/signup", async (req, res) => {
         }
     }
 });
+
+router.post("/auth/signin", async (req, res) => {
+    const user = await verifyCredentials(
+        req.body.email_or_username,
+        req.body.password
+    );
+
+    try {
+        if (user) {
+            const user_object = await generateUserObject(user);
+            res.status(200).send({
+                user: user_object,
+                message: "User logged in successfully."
+            });
+        } else {
+            res.status(401).send({
+                message: "The enetered credentials are invalid."
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message:
+                "The server encountered an unexpected condition which prevented it from fulfilling the request."
+        });
+    }
+});
+
 module.exports = router;
