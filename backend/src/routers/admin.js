7711697eaed90f5efeaa4202330070409568858c
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2022-12-30 23:48:50 +0200

    Created API to remove user from the database

--- backend/src/routers/admin.js
+++ backend/src/routers/admin.js
@@ -22,5 +22,24 @@ router.get("/users/list", async (req, res) => {
     }
 });
 
+router.delete("/user/remove", async (req, res) => {
+    try {
+        const username = req.body.user_name;
+        const user = await User.findOneAndDelete({ user_name: username });
+        if (!user) {
+            return res.status(404).send({
+                message: "User not found."
+            });
+        }
+        res.status(200).send({
+            message: "User removed."
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: error.toString()
+        });
+    }
+});
+
 
 module.exports = router;
