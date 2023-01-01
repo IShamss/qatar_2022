35e27564d905735453f4fe511a300c9ead8a5952
ahmedelgarf94 <ahmedelgarf94@gmail.com>
2023-01-01 23:00:09 +0200

    Created API for reservation of matches

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const User = require("../models/User.js");
+const Match = require("../models/Match");
+const Reservation = require("../models/Reservation");
+const Stadium = require("../models/Stadium");
 const router = express.Router();
 
 router.patch("/user/:id", async (req, res) => {
@@ -44,6 +47,76 @@ router.patch("/user/:id", async (req, res) => {
             });
         }
     }
+});
+
+router.post("/reservation", async (req, res) => {
+    try {
+        const reservation = new Reservation(req.body);
+        const existing_reservation = await Reservation.findOne({
+            match: reservation.match,
+            user: reservation.user,
+            place: reservation.place
+        });
+        if (existing_reservation) {
+            return res.status(409).send({
+                message: "Reservation already exists with the same data.",
+            });
+        }
+        const all_reservations = await Reservation.find({
+            match: reservation.match,
+        });
+        if (all_reservations) {
+            const found_reservation = all_reservations.find(element => element.place == reservation.place);
+            if (found_reservation) {
+                return res.status(409).send({
+                    message: "Reservation already exists for another user.",
+                });
+            }
+        }
+        const match = await Match.findById(reservation.match);
+        if (!match) {
+            return res.status(404).send({
+                message: "Match not found.",
+            });
+        } else {
+            const stadium = await Stadium.findById(match.stadium);
+            if (!stadium) {
+                return res.status(404).send({
+                    message: "Stadium not found.",
+                });
+            } else {
+                const stadium_places = stadium.length * stadium.width;
+                if (reservation.place < 1 || reservation.place > stadium_places) {
+                    return res.status(409).send({
+                        message: "Invalid ticket number. number should be between 1 and " + stadium_places,
+                    });
+                }
+            }
+        }
+        const saved_reservation = await reservation.save();
+        if (saved_reservation) {
+            res.status(200).send({
+                message: "Reservation created.",
+                reservation: reservation,
+            });
+        } else {
+            res.status(400).send({
+                message: "Reservation saving failed.",
+            });
+        }
+
+    } catch (error) {
+        if (error.name == "ValidationError") {
+            res.status(400).send({
+                message: "Validation error: " + error.message
+            });
+        } else {
+            res.status(500).send({
+                message: "Server error: " + error.message
+            });
+        }
+    }
+
 });
 
 module.exports = router;
